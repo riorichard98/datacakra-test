350e34232156ec8e01da359a6978e772c9d3877e
riorichard98 <riorichard12@gmail.com>
2025-07-31 16:30:40 +0700

    feat: user not found message

--- src/constants/general-error-message.ts
+++ src/constants/general-error-message.ts
@@ -1,4 +1,5 @@
 export const GENERAL_ERROR_MESSAGE = {
     DATA_NOT_EXISTS: 'data tidak ditemukan',
-    UNAUTHORIZED: 'unauthorized'
+    UNAUTHORIZED: 'unauthorized',
+    USER_NOT_FOUND: 'user tidak ditemukan'
 }
