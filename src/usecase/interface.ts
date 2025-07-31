c2690c49b9f1a35284223b28f0476dd58b84edea
riorichard98 <riorichard12@gmail.com>
2025-07-31 16:31:17 +0700

    feat: returned data interface in usecase

---
+++ src/usecase/interface.ts
@@ -0,0 +1,41 @@
+export interface LoginResponseData {
+    token: string
+}
+
+export interface UpsertTouristResponseData {
+    userId: string
+    fullName: string
+    email: string
+}
+
+export interface DeleteTouristResponseData {
+    deletedUserId: string
+}
+
+export type ListTouristResponseData = {
+    userId: string
+    fullName: string
+    email: string
+}[]
+
+export interface UpdateTripResponseData {
+    updatedTripId: string
+}
+
+export interface InsertTripResponseData {
+    destination: string
+    endDate: Date
+    startDate: Date
+    tripId: string
+}
+
+export interface DeleteTripResponseData {
+    deletedTripId: string
+}
+
+export type ListTripResponseData = {
+    destination: string
+    endDate: Date
+    startDate: Date
+    tripId: string
+}[]
